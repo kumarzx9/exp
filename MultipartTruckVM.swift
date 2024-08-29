cbd4456bcb9d27884a51d925f71b188dea47b40e
kumarzx9 <144837201+kumarzx9@users.noreply.github.com>
2024-08-29 13:13:12 +0530

    Update MultipartTruckVM.swift

--- MultipartTruckVM.swift
+++ MultipartTruckVM.swift
@@ -10,22 +10,6 @@ class MultipartTruckVM {
     var vidUploadData: VideoUploadModal?
     
     // MARK: almofire chorwtux create or edit truck
-    func createTruck(parameters: [String: Any], profileImage1: UIImage?, profileImgName1: String, profileImage2: UIImage?, profileImgName2: String, imagesArray: [UIImage], completion: @escaping (_ result: MultipartTruckModal?, _ error: Error?) -> ()) {
-        let token = ApiEndpoints.token
-        let headers: HTTPHeaders = ["Authorization": "Bearer \(token ?? "")"]
-        APIServices.shared.alamofire_uploadImages(url: ApiEndpoints.createTruck, method: .post, parameters: parameters, profileImage1: profileImage1, profileImgName1: profileImgName1, profileImage2: profileImage2, profileImgName2: profileImgName2, imagesArray: imagesArray, headers: headers) { result, error in
-            if let result = result {
-               // debugPrint("Success: \(result)")
-                self.multipartTruckData = result as? MultipartTruckModal
-                completion(result as? MultipartTruckModal, nil)
-            } else if let error = error {
-                //print("Error: \(error)")
-                self.multipartTruckData = nil
-                completion(nil, error)
-            }
-        }
-        
-    }
     
      // MARK: media Upload Url session // image or video with optional image array
     func mediaUpload(parms: [String: Any],
