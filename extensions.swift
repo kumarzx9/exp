09a0f5efcce73d6acecc022bcbc90892283dcf31
kumarzx9 <144837201+kumarzx9@users.noreply.github.com>
2024-09-03 20:33:49 +0530

    Update extensions.swift

--- extensions.swift
+++ extensions.swift
@@ -1,4 +1,3 @@
-
 import Foundation
 import UIKit
 
@@ -82,17 +81,52 @@ extension UIView {
         }
     }
     
-// MARK: - Add shadow
+    // MARK: - Add shadow
     func addBottomShadow() {
-       //  Set up the shadow properties
-            layer.shadowColor = UIColor.darkGray.cgColor
-            layer.shadowOpacity = 0.5
-            layer.shadowOffset = CGSize(width: 0, height: 4)
-            layer.shadowRadius = 4
-            // Create a UIBezierPath for the shadow
-             let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: bounds.height - 4, width: bounds.width + 40, height: 4))
-           layer.shadowPath = shadowPath.cgPath
+        //  Set up the shadow properties
+        layer.shadowColor = UIColor.darkGray.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 4
+        // Create a UIBezierPath for the shadow
+        let shadowPath = UIBezierPath(rect: CGRect(x: 0, y: bounds.height - 4, width: bounds.width + 40, height: 4))
+        layer.shadowPath = shadowPath.cgPath
     }
+}
+
+func isValidEmail(_ email: String) -> Bool {
+    let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+    let emailPred = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+    return emailPred.evaluate(with: email)
+}
+
+// MARK: UIViewController extensions
+extension UIViewController {
+    
+    func dispalyAlert(message: String) {
+        let alert = UIAlertController(title: "", message: message, preferredStyle: UIAlertController.Style.alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alert, animated: true, completion: nil)
+    }
+    
+    func showAlertWithOkHadler(title: String, message: String, completion: (() -> Void)? = nil) {
+        let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK", style: .default) { _ in
+            // Call completion handler if provided
+            completion?()
+        }
+        alertController.addAction(okAction)
+        // Get the root view controller to present the alert
+        if let rootViewController = UIApplication.shared.windows.first?.rootViewController {
+            rootViewController.present(alertController, animated: true, completion: nil)
+        }
+    }
+}
+
+func setRightPadding(for textField: UITextField, paddingWidth: CGFloat) {
+    let rightPaddingView = UIView(frame: CGRect(x: 0, y: 0, width: paddingWidth, height: textField.frame.height))
+    textField.rightView = rightPaddingView
+    textField.rightViewMode = .always
 }
 
 
